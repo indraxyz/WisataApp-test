04b225fbe0603c7d6abaf75c8f7abf3cd933ad84
indracahyae <indracahyae@gmail.com>
2024-12-02 12:23:32 +0700

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 // import Image from "next/image";
 import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Grid2 as Grid from "@mui/material/Grid2";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <Container maxWidth="lg">
       <Button variant="outlined">hello world</Button>
-    </div>
+      {/* top nav */}
+      {/* selected place
+            OR
+          property 
+      */}
+      {/* list offer 
+        	place : tab menu (stay) 
+          OR
+          property : tab menu (Deals, Photos, Info)
+      */}
+    </Container>
   );
 }
