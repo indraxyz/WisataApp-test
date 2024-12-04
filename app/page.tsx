85d6aae2c9329a170a85c3de5acb232f74e6ca34
indracahyae <indracahyae@gmail.com>
2024-12-04 12:23:27 +0700

    search

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 "use client";
 import { useState } from "react";
 
-import { Tab, Tabs, Dialog, DialogContent } from "@mui/material";
+import {
+  Tab,
+  Tabs,
+  Dialog,
+  DialogContent,
+  TextField,
+  InputAdornment,
+  Input,
+  Autocomplete,
+} from "@mui/material";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import Container from "@mui/material/Container";
@@ -22,6 +31,12 @@ import AssignmentIndIcon from "@mui/icons-material/AssignmentInd";
 import LocalOfferIcon from "@mui/icons-material/LocalOffer";
 import CollectionsIcon from "@mui/icons-material/Collections";
 import InfoIcon from "@mui/icons-material/Info";
+import GoogleIcon from "@mui/icons-material/Google";
+import AppleIcon from "@mui/icons-material/Apple";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import SearchIcon from "@mui/icons-material/Search";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import ClearIcon from "@mui/icons-material/Clear";
 
 import Logo from "../public/logo.png";
 import ImgSquare from "../public/img-square.jpg";
@@ -96,8 +111,9 @@ export default function Home() {
           <Button
             variant="contained"
             fullWidth
-            sx={{ bgcolor: grey[200], color: "black" }}
+            sx={{ bgcolor: grey[100], color: "black" }}
             onClick={() => setDialogSearchBar(true)}
+            startIcon={<SearchIcon />}
           >
             Search
           </Button>
@@ -216,27 +232,128 @@ export default function Home() {
 
       {/* DIALOG SIGN IN */}
       <Dialog
+        maxWidth="xs"
         open={dialogSignin}
         onClose={() => setDialogSignin(false)}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
         <DialogContent>
-          SIGN IN Let Google help apps determine location. This means sending
-          anonymous location data to Google, even when no apps are running.
+          {/* sign in form or sosmed acc */}
+          <Box component={"div"} className="space-y-4">
+            <Typography className="text-lg text-center">
+              Sign in to Wisata App
+            </Typography>
+            <TextField fullWidth label="Username or Email" variant="standard" />
+            <Button fullWidth variant="contained">
+              Continue
+            </Button>
+          </Box>
+
+          <Divider variant="fullWidth" className="my-4">
+            <Typography className="text-sm">or continue with</Typography>
+          </Divider>
+          <Box component="div" className="pb-4 text-center space-x-4">
+            <IconButton color="inherit">
+              <GoogleIcon />
+            </IconButton>
+            <IconButton color="inherit">
+              <AppleIcon />
+            </IconButton>
+            <IconButton color="inherit">
+              <WhatsAppIcon />
+            </IconButton>
+          </Box>
+          <Divider>
+            <Typography className="text-sm">don't have an account ?</Typography>
+          </Divider>
+          {/* create new acc */}
+          <Button variant="outlined" fullWidth className="mt-4">
+            Create Account
+          </Button>
         </DialogContent>
       </Dialog>
 
       {/* DIALOG SEARCH */}
       <Dialog
+        fullWidth
+        maxWidth={"xs"}
         open={dialogSearchBar}
         onClose={() => setDialogSearchBar(false)}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
         <DialogContent>
-          SEARCH BAR Let Google help apps determine location. This means sending
-          anonymous location data to Google, even when no apps are running.
+          <Typography className="mb-4 text-lg">
+            Choose your beautiful trip 🌈
+          </Typography>
+          {/* responsive grid column */}
+          <Grid container spacing={2} columns={{ xs: 4 }}>
+            {/* location */}
+            <Grid size={{ xs: 4 }}>
+              <Autocomplete
+                className="w-full"
+                freeSolo
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="standard"
+                    label="Where are you going ?"
+                    placeholder="Search hotels, apartments, villas"
+                    slotProps={{
+                      input: {
+                        ...params.InputProps,
+                        startAdornment: (
+                          <InputAdornment position="start">
+                            <LocationOnIcon />
+                          </InputAdornment>
+                        ),
+                      },
+                    }}
+                  />
+                )}
+                value={""}
+                options={[]}
+                // onKeyUp={suggestSearch}
+                // onKeyDown={enterSearch}
+                // onInputChange={(e, v) => console.log("")}
+                // onChange={() => console.log()}
+              />
+            </Grid>
+            {/* date in, out,*/}
+            <Grid container spacing={{ xs: 2, sm: 4 }}>
+              <Grid size={{ xs: 4, sm: 2 }}>
+                <TextField
+                  fullWidth
+                  label="Where are you going ?"
+                  variant="standard"
+                />
+              </Grid>
+              <Grid size={{ xs: 4, sm: 2 }}>
+                <TextField
+                  fullWidth
+                  label="Where are you going ?"
+                  variant="standard"
+                />
+              </Grid>
+            </Grid>
+
+            {/* guest room */}
+            <Grid container spacing={{ xs: 2, sm: 4 }}>
+              <Grid size={{ xs: 4, sm: 2 }}>
+                <TextField fullWidth label="Guest ?" variant="standard" />
+              </Grid>
+              <Grid size={{ xs: 4, sm: 2 }}>
+                <TextField fullWidth label="Room ?" variant="standard" />
+              </Grid>
+            </Grid>
+            {/* submit */}
+            <Grid size={{ xs: 4 }}>
+              <Button fullWidth variant="contained" startIcon={<SearchIcon />}>
+                Search
+              </Button>
+            </Grid>
+          </Grid>
         </DialogContent>
       </Dialog>
     </Container>
